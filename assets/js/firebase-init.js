c0811bf35fe7905edb82e293bb71b03df0266de5
DK-PRIME <d.n.i.o.n.i.r@gmail.com>
2025-12-10 21:13:20 +0200

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,22 +1,25 @@
 // assets/js/firebase-init.js
-// ЄДИНА ініціалізація Firebase для STOLAR CARP + DK Prime (MODULAR SDK)
+// Підключається ПІСЛЯ compat-скриптів Firebase з CDN
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+(function () {
+  // Конфіг твого проекту STOLAR CARP
+  const firebaseConfig = {
+    apiKey: "AIzaSyBU7BSwGl0laDvHGhrvu14nJWpabsjSoNo",
+    authDomain: "stolar-carp.firebaseapp.com",
+    projectId: "stolar-carp",
+    storageBucket: "stolar-carp.firebasestorage.app",
+    messagingSenderId: "1019636788370",
+    appId: "1:1019636788370:web:af1c1ecadb683df212ca4b",
+    // measurementId можна не чіпати
+  };
 
-// ТВОЯ КОНФІГРАЦІЯ
-const firebaseConfig = {
-  apiKey: "AIzaSyBU7BSwGl0laDvHGhrvu14nJWpabsjSoNo",
-  authDomain: "stolar-carp.firebaseapp.com",
-  projectId: "stolar-carp",
-  storageBucket: "stolar-carp.firebasestorage.app",
-  messagingSenderId: "1019636788370",
-  appId: "1:1019636788370:web:af1c1ecadb683df212ca4b",
-  measurementId: "G-VWC07QNS7P"
-};
+  // Ініціалізація (перевірка, щоб не інітити двічі)
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
-// 1 додаток на проект
-export const app  = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db   = getFirestore(app);
+  // Глобальні змінні для всіх твоїх скриптів
+  window.auth = firebase.auth();
+  window.db = firebase.firestore();
+  window.storage = firebase.storage();
+})();

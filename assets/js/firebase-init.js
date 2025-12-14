b1bd359119e3d4f4dd0115bc8f62e5654f467aa3
DK-PRIME <d.n.i.o.n.i.r@gmail.com>
2025-12-14 12:39:24 +0200

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -3,7 +3,6 @@
 // Використовуємо compat-версію SDK (10.12.2) і глобальний об'єкт firebase.
 
 (function () {
-  // Конфігурація саме твого проекту STOLAR CARP
   const firebaseConfig = {
     apiKey: "AIzaSyBU7BSwGl0laDvHGhrvu14nJWpabsjSoNo",
     authDomain: "stolar-carp.firebaseapp.com",
@@ -13,22 +12,19 @@
     appId: "1:1019636788370:web:af1c1ecadb683df212ca4b"
   };
 
-  // Ініціалізуємо додаток лише один раз
   if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
 
-  // Базові сервіси
   const auth = firebase.auth();
   const db = firebase.firestore();
   const storage = firebase.storage();
 
-  // Глобальні посилання для ВСЬОГО проекту
   window.scAuth = auth;
   window.scDb = db;
   window.scStorage = storage;
 
-  // Для сумісності, якщо десь уже використовуєш ці імена
+  // сумісність
   window.auth = auth;
   window.db = db;
   window.storage = storage;
